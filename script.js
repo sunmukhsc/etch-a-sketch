0f5fa1a06da78633efec92990a06e70a8e65e1c0
Sunmukh <sunmukhsc@gmail.com>
2023-09-27 19:21:32 -0700

    Add button for custom grid sizes

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const gridPixelWidth = 16;
+const containerPixels = 600;
+const maxPixels = 64;
+const gridPixels = 16;
 const container = document.querySelector('.container');
+const sizeBtn = document.querySelector('#size');
 
-for (let i = 0; i < gridPixelWidth**2; i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    square.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'black';
-    });
-    container.appendChild(square);
+function setGridSize() {
+    input = prompt(`Enter a custom grid size: (max ${maxPixels})`);
+    if (input && input<=maxPixels && input>0) {
+        container.innerHTML = '';
+        createGrid(input);
+    } else {return;}
+    
 }
+
+function createGrid (gridPixels) {
+    for (let i = 0; i < gridPixels**2; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.style.width = 
+            `${containerPixels/gridPixels}px`;
+        square.style.height = 
+            `${containerPixels/gridPixels}px`;
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'black';
+        });
+        container.appendChild(square);
+    }
+}
+
+createGrid(gridPixels);
+
+sizeBtn.addEventListener('click', setGridSize);
+
+container.style.width = `${containerPixels}px`;
+container.style.height = `${containerPixels}px`;
+
