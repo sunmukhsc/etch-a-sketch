312dda1a7ec21ddc3f893e9ecee0a3404d2bc33b
Sunmukh <sunmukhsc@gmail.com>
2023-09-27 20:22:49 -0700

    Add button to draw with random colors

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const containerPixels = 600;
-const maxPixels = 64;
-const gridPixels = 16;
-const container = document.querySelector('.container');
-const sizeBtn = document.querySelector('#size');
-
 function setGridSize() {
     input = prompt(`Enter a custom grid size: (max ${maxPixels})`);
     if (input && input<=maxPixels && input>0) {
@@ -13,7 +7,7 @@ function setGridSize() {
     
 }
 
-function createGrid (gridPixels) {
+function createGrid (gridPixels, isRgb=false) {
     for (let i = 0; i < gridPixels**2; i++) {
         const square = document.createElement('div');
         square.className = 'square';
@@ -21,17 +15,46 @@ function createGrid (gridPixels) {
             `${containerPixels/gridPixels}px`;
         square.style.height = 
             `${containerPixels/gridPixels}px`;
-        square.addEventListener('mouseenter', () => {
-            square.style.backgroundColor = 'black';
-        });
+        if (isRgb) {
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = randomRgbString();
+            });
+        } else {
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = 'black';
+            });
+        }
         container.appendChild(square);
     }
 }
 
-createGrid(gridPixels);
 
-sizeBtn.addEventListener('click', setGridSize);
+function randomRgbString() {
+    return `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
+}
+
+function randNum() {
+    return Math.floor(Math.random() * 255);
+}
+
+const containerPixels = 600;
+const maxPixels = 64;
+const gridPixels = 16;
+const container = document.querySelector('.container');
+const sizeBtn = document.querySelector('#size');
+const rgbBtn = document.querySelector('#rgb');
 
 container.style.width = `${containerPixels}px`;
 container.style.height = `${containerPixels}px`;
 
+createGrid(gridPixels);
+
+sizeBtn.addEventListener('click', setGridSize);
+rgbBtn.addEventListener('click', () => {
+    
+    container.innerHTML = '';
+    const isRgb = true;
+    createGrid(gridPixels, isRgb);
+        
+    square.style.backgroundColor = randomRgbString();
+});
